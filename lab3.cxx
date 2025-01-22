8a0da04e9a640f49fc2bcbaee9bb4eccaa5161ce
tidmkhokhlov <tidmkhokhlov@edu.hse.ru>
2025-01-22 18:08:52 +0300

    producer->cunsumer

--- lab3.cxx
+++ lab3.cxx
@@ -1,30 +1,55 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <cstring>
 #include <fstream>
 #include <thread>
 #include <mutex>
 #include <algorithm>
+#include <queue>
+#include <condition_variable>
+
+const int NUM_CONSUMERS = 4;
 
 using namespace std;
 
 mutex mtx;
 
-void processTextChunk(const vector<string> &chunk) {
-    for (string line : chunk) {
-        for (int i = 0; i < line.length(); i++) {
-            line[i] = toupper(line[i]);
-        }
-
-        lock_guard<mutex> lock(mtx);
-        cout << "Thread" << "[" << this_thread::get_id() << "]" << "  " << line << endl;
+template <typename T>
+class blockingQueue{
+  private:
+    queue<T> bqueue;
+    mutex bq_mtx;
+    condition_variable cv;
+  public:
+    void push(T item) {
+        unique_lock<mutex> lock(bq_mtx);
+        bqueue.push(item);
+        cv.notify_one();
+    } 
+    T pop() {
+        unique_lock<mutex> lock(bq_mtx);
+        cv.wait(lock, [this]() {return !bqueue.empty(); });
+        T item = bqueue.front();
+        bqueue.pop();
+        return item;
     }
-}
+    bool empty() {
+        if (bqueue.empty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
 
-int main() {
+void producer(const string filePath, blockingQueue<vector<string>>& b_queue) {
+    {
+        lock_guard<mutex> lock(mtx);
+        cout << "Producer started" << endl;
+    }
+
     vector<string> data;
-    ifstream inputFile("./input.txt");
+    ifstream inputFile(filePath);
     if (inputFile.is_open()) {
         string line;
         while (getline(inputFile, line)) {
@@ -32,24 +57,51 @@ int main() {
         }
         inputFile.close();
     } else {
-        cout << "\033[31mFailed to open the file\033[0m" << endl;
-        return 0;
+        lock_guard<mutex> lock(mtx);
+        cerr << "Failed to open the file\n";
+        return;
     }
 
-    const int num_threads = 4;
-    int chunk_size = ((data.size() + num_threads - 1) / num_threads);
-
-    vector<thread> threads;
-    for (int i = 0; i < num_threads; ++i) {
+    int chunk_size = ((data.size() + NUM_CONSUMERS - 1) / NUM_CONSUMERS);
+    for (int i = 0; i < NUM_CONSUMERS; ++i) {
         int start = i * chunk_size;
         int end = min(start + chunk_size, int(data.size()));
         vector<string> chunk(data.begin() + start, data.begin() + end);
-        threads.emplace_back(processTextChunk, chunk);
+        {
+            lock_guard<mutex> lock(mtx);
+            cout << "Producer added a chunk to the blocking queue" << endl;
+        }
+        b_queue.push(chunk);
+    }
+    
+}
+
+void consumer(blockingQueue<vector<string>>& b_queue) {
+    if (!b_queue.empty()) {
+        for (string line : b_queue.pop()) {
+            for (int i = 0; i < line.length(); i++) {
+                line[i] = toupper(line[i]);
+            }
+            {
+                lock_guard<mutex> lock(mtx);
+                cout << "Consumer" << "[" << this_thread::get_id() << "]" << "  " << line << endl;
+            }
+        }
+    }
+}
+
+int main() {
+    const string inputFile = "./input.txt";
+    blockingQueue<vector<string>> b_queue;
+    producer(inputFile, b_queue);
+    vector<thread> consumers;
+    for (int i = 0; i < NUM_CONSUMERS; i++) {
+        consumers.emplace_back(consumer, ref(b_queue));
     }
 
-    for (auto& thread : threads) {
-        if (thread.joinable()) {
-            thread.join();
+    for (auto& consumer : consumers) {
+        if (consumer.joinable()) {
+            consumer.join();
         }
     }
 
